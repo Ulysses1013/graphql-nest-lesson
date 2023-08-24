4a2424057b3907a7c4e4b273313e0aededbd6d68
Ulysses1013 <kanaina99102@gmail.com>
2023-08-24 09:47:23 +0900

    Jwt認証を追加

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -4,6 +4,8 @@ import { CreateUserInput } from './dto/createUser.input';
 import { User } from '@prisma/client';
 import { User as UserModel } from './models/user.model';
 import { GetUserArgs } from './dto/getUser.args';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver()
 export class UserResolver {
@@ -17,6 +19,7 @@ export class UserResolver {
   }
 
   @Query((returns) => UserModel, { nullable: true })
+  @UseGuards(JwtAuthGuard) // request headerにtokenが含まれている場合のみ実行される
   async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
     return await this.userService.getUser(getUserArgs.email);
   }
