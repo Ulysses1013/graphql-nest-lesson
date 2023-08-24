9651a4e4024a7e783b221355f65ae20013254c79
Ulysses1013 <kanaina99102@gmail.com>
2023-08-24 16:31:35 +0900

    routerを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import { GuestRoute, PrivateRoute } from "./AuthRoute";
 
 function App() {
   return (
-    <>
-      <div>
-        <SignIn />
-        <SignUp />
-        <Main />
-        <NotFound />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<GuestRoute children={<SignIn />} />} />
+        <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
+        <Route path="/" element={<PrivateRoute children={<Main />} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
